ff57d2a811afa637ebe1096d204ee0f9e6663b34
ADRIEN BARDES <adrien.bardes@etu.u-pec.fr>
2018-04-10 19:07:59 +0200

    No more minutes in date time

--- src/main/scala/logic/items/production/FactoryFactory.scala
+++ src/main/scala/logic/items/production/FactoryFactory.scala
@@ -16,46 +16,46 @@ object FactoryFactory {
       case GRAIN_FARM =>
         new Factory(GRAIN_FARM, company, town,
           ListBuffer(
-            Recipe(List(), (GRAIN, 5000), new DateTime(4, 0, 0))
+            Recipe(List(), (GRAIN, 5000), new DateTime(4, 0))
           )
         )
 
       case GARDEN =>
         new Factory(GARDEN, company, town,
           ListBuffer(
-            Recipe(List(), (VEGETABLE, 10000), new DateTime(6, 0, 0)),
-            Recipe(List(), (FRUIT, 10000), new DateTime(6, 0, 0))
+            Recipe(List(), (VEGETABLE, 10000), new DateTime(30, 0)),
+            Recipe(List(), (FRUIT, 10000), new DateTime(30, 0))
           )
         )
 
       case IRON_MINE =>
         new Factory(IRON_MINE, company, town,
           ListBuffer(
-            Recipe(List(), (IRON, 350), new DateTime(1, 0, 0))
+            Recipe(List(), (IRON, 350), new DateTime(1, 0))
           )
         )
 
       case BAUXITE_MINE =>
         new Factory(BAUXITE_MINE, company, town,
           ListBuffer(
-            Recipe(List(), (BAUXITE, 100), new DateTime(1, 0, 0))
+            Recipe(List(), (BAUXITE, 100), new DateTime(1, 0))
           )
         )
 
       case COAL_MINE =>
         new Factory(COAL_MINE, company, town,
           ListBuffer(
-            Recipe(List(), (COAL, 1500), new DateTime(0, 12, 0))
+            Recipe(List(), (COAL, 1500), new DateTime(0, 12))
           )
         )
 
       case TEXTILE_FACTORY =>
         new Factory(TEXTILE_FACTORY, company, town,
           ListBuffer(
-            Recipe(List((IRON, 10)), (TEXTILE, 1), new DateTime(3, 0, 0)),
-            Recipe(List((BAUXITE, 5)), (TEXTILE, 1), new DateTime(3, 0, 0)),
-            Recipe(List((BAUXITE, 10)), (LEATHER, 1), new DateTime(3, 0, 0)),
-            Recipe(List((LEATHER, 3)), (PAPER, 1), new DateTime(3, 0, 0))
+            Recipe(List((IRON, 10)), (TEXTILE, 1), new DateTime(3, 0)),
+            Recipe(List((BAUXITE, 5)), (TEXTILE, 1), new DateTime(3, 0)),
+            Recipe(List((BAUXITE, 10)), (LEATHER, 1), new DateTime(3, 0)),
+            Recipe(List((LEATHER, 3)), (PAPER, 1), new DateTime(3, 0))
           )
         )
 
